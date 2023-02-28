07228180476fea2d29363a66caa0b9409da61080
Zyran <zyran.dev@gmail.com>
2023-02-27 19:40:46 -0600

    Initial commit

---
+++ pkg/cmd/main_command.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"gopkg.in/yaml.v3"
+	"ignis/pkg/builder"
+	. "ignis/pkg/repository"
+	"ignis/pkg/template"
+	"os"
+)
+
+func init() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+}
+
+type Configuration struct {
+	Repositories []Repository `yaml:"repositories"`
+}
+
+func main() {
+	var configPath, indexTemplatePath, repoTemplatePath, outputDir string
+
+	flag.StringVar(&configPath, "config", "config.yml", "Configuration file path")
+	flag.StringVar(&indexTemplatePath, "index", "templates/index.html", "Index template path")
+	flag.StringVar(&repoTemplatePath, "repo", "templates/repository.html", "Repository template path")
+	flag.StringVar(&outputDir, "output", "build", "Repository template path")
+
+	flag.Parse()
+
+	indexTemplate, err := template.ReadTemplate(indexTemplatePath)
+	if err != nil {
+		log.Info().Err(err)
+		return
+	}
+	repoTemplate, err := template.ReadTemplate(repoTemplatePath)
+	if err != nil {
+		log.Info().Err(err)
+		return
+	}
+	ignisBuilder := builder.NewBuilder(indexTemplate, repoTemplate)
+	config, err := readConfiguration(configPath)
+	err = ignisBuilder.Build(outputDir, config.Repositories)
+	if err != nil {
+		log.Info().Err(err)
+		return
+	}
+	log.Info().Msg("Successfully generated ignis files.")
+}
+
+func readConfiguration(path string) (config *Configuration, err error) {
+	fileContent, err := os.ReadFile(path)
+	if err != nil {
+		return
+	}
+	config = &Configuration{}
+	err = yaml.Unmarshal(fileContent, config)
+	return
+}
